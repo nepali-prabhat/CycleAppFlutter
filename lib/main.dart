a27c6c8d50455cf0b25230ad1b01f18a8f84ad62
karun777 <karun777@outlook.com>
2019-06-01 13:20:15 +0545

    changes made

--- lib/main.dart
+++ lib/main.dart
@@ -5,38 +5,32 @@ import 'package:cycle_app/Service/userService.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
-
 GetIt getIt = new GetIt();
 
 void main() {
-    getIt.registerSingleton(UserService());
-    getIt.registerSingleton(LocationService());
-    runApp(MaterialApp(
-        title: "The Cycle App", home: MyApp()
-    ));
+  getIt.registerSingleton(UserService());
+  getIt.registerSingleton(LocationService());
+  runApp(MaterialApp(title: "The Cycle App", home: MyApp()));
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   final UserService userService = getIt.get<UserService>();
   @override
   Widget build(BuildContext context) {
-    
     return StreamBuilder(
-            stream: userService.isLoggedIn$,
-            builder:(context, snapshot){
-                if(snapshot.connectionState == ConnectionState.waiting){
-                    return Scaffold(body: Center(child: Text('loading screen here.')));
-                }
-                if(snapshot.connectionState == ConnectionState.active){
-                    bool loggedIn = snapshot.data;
-                    if(loggedIn){
-                        return MyHome();
-                    }else{
-                        return NewLogin();
-                    }
-                }
-        }
-    );
+        stream: userService.isLoggedIn$,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(body: Center(child: Text('loading screen here.')));
+          }
+          if (snapshot.connectionState == ConnectionState.active) {
+            bool loggedIn = snapshot.data;
+            if (loggedIn) {
+              return Home();
+            } else {
+              return NewLogin();
+            }
+          }
+        });
   }
-  
 }
